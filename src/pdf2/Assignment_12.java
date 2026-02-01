0f261302770c4f6433dfc8f81fcb8df9a47f8bb4
pradeepxvi <pradeepxvi@gmail.com>
2026-02-01 22:30:18 +0545

    added more

--- src/pdf2/Assignment_12.java
+++ src/pdf2/Assignment_12.java
@@ -5,24 +5,48 @@ class BCA {
     int rollNo;
     double marks;
 
-    double percentage = (marks / 5) * 100;
-    char grade;
+    BCA(String name, int rollNo, double marks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
 
-    double calculateGrade() {
-        switch (percentage) {
-            case (percentage > 80 && percentage <= 100):
-                grade = 'A';
-                break;
-
-            default:
-                break;
+    char calculateGrade() {
+        char grade = ' ';
+        if (marks <= 100 && marks > 80) {
+            grade = 'A';
+        } else if (marks <= 80 && marks > 60) {
+            grade = 'B';
+        } else if (marks <= 60 && marks > 40) {
+            grade = 'C';
+        } else {
+            grade = 'F';
         }
+
+        return grade;
+    }
+
+    void getGrade() {
+        System.out.println("\n\nname = " + this.name);
+        System.out.println("roll no = " + this.rollNo);
+        System.out.println("marks = " + this.marks);
+        System.out.println("grade = " + calculateGrade());
     }
 
 }
 
 public class Assignment_12 {
     public static void main(String[] args) {
+
+        BCA s1 = new BCA("KP Oli hanyo goli", 1, 98);
+        BCA s2 = new BCA("Parchande hatyara", 2, 75);
+        BCA s3 = new BCA("Sere chor", 3, 56);
+        BCA s4 = new BCA("Jyoti magar ", 4, 10);
+
+        s1.getGrade();
+        s2.getGrade();
+        s3.getGrade();
+        s4.getGrade();
 
     }
 
